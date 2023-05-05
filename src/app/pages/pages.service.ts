6301f4cb28a10c7e3f9dda292510be4535a8e013
MaximeDemougin <demougin.maxime1@gmail.com>
2023-05-05 12:21:52 +0200

    fini

--- src/app/pages/pages.service.ts
+++ src/app/pages/pages.service.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class endpointService {
-  public endpoint: string = 'http://127.0.0.1:5000';
-  // public endpoint:string = 'https://footnetapi.onrender.com'
+  // public endpoint: string = 'http://127.0.0.1:5000';
+  public endpoint:string = 'https://footnetapi.onrender.com'
   //public endpoint:string = 'https://maximedemougin-sturdy-space-funicular-xxgp7jq49vg2pgjg-5000.preview.app.github.dev'
   
   getEndpoint() {
